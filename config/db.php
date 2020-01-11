1ae2b33f5416393f41742807ddb70afe948b2c9b
Slava <neo098765@gmail.com>
2020-01-11 20:12:14 +0300

    add migration m200108_111941_create_user_table.php, override config

--- config/db.php
+++ config/db.php
@@ -1,8 +1,15 @@
 <?php
 
-return [
+$db = [];
+
+$ovDB = __DIR__ . '/override/db.php';
+if (file_exists($ovDB)) {
+    $db = require $ovDB;
+}
+
+return $db + [
     'class' => 'yii\db\Connection',
-    'dsn' => 'mysql:host=localhost;dbname=yii2basic',
+    'dsn' => 'mysql:host=localhost;dbname=social_web',
     'username' => 'root',
     'password' => '',
     'charset' => 'utf8',
