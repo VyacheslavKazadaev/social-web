e61abf7d39fd0c3ddc4159dd6a8b35afc0f02cc3
vkazadaev <vkazadaev@buka.ru>
2020-01-22 18:11:08 +0300

    change: delete custom config

--- config/override/db.php
+++
@@ -1,9 +0,0 @@
-<?php
-return [
-    'username' => 'root',
-    'password' => 'aladen',
-
-//    'username' => 'u236269',
-//    'password' => 'co7p6blell',
-//    'dsn' => 'mysql:host=u236269.mysql.masterhost.ru;dbname=u236269_socialwb',
-];
