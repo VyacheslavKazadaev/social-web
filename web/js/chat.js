820460aef1fe8793acbf11f7bd85a78bf1ad91c4
Slava <neo098765@gmail.com>
2020-03-14 15:56:27 +0300

    fix save write to chat

--- web/js/chat.js
+++ web/js/chat.js
@@ -3,15 +3,20 @@ $(document)
 ;
 
 async function writeInChat() {
+    let message = $('#btn-input').val().trim();
+    if (message === '') {
+        return;
+    }
+
     let response = await fetch('', {
         method: 'POST',
         headers: {
             'Content-Type': 'application/x-www-form-urlencoded;charset=utf-8'
         },
-        body: `message=${$('#btn-input').val()}&${yii.getCsrfParam()}=${yii.getCsrfToken()}`
+        body: `message=${message}&${yii.getCsrfParam()}=${yii.getCsrfToken()}`
     });
 
     let result = await response.text();
-    console.log(result);
     $('ul.chat').append(result);
+    document.location.reload();
 }
